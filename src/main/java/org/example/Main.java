e683ff8c79fc32578d6238ece2496bb64302c102
Gyömbér Judit <gyomberjudit@gmail.com>
2023-03-18 20:51:38 +0100

    initial2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,39 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    }
+
+// angol Stringben számoljuk meg a benne lévő mássalhangzókat
+// ha számlálni akarunk, kel egy számláló
+    //megszámoljuk a magánhangzókat és kivonjuk a string hosszából
+    public int countConsonent(String str) {
+        int count = 0;
+        String vowel = "aeiou";                     //nagybetűt is figyelni kell!, ezért átalakítjuk a bejövő str-t kisbetűssé, a ciklus előtt
+        str = str.toLowerCase();                    //ugyanaz a változó neve, str
+        for (int i=0; i< str.length(); i++) {       //megszámolni a magánhangzókat, egyenként
+            char n = str.charAt(i);                 // karaktereken végigmenni és elmenteni egy változóba
+            if (vowel.indexOf(n) < 0) {             // indexOf, megmondja, hogy az adott karakter melyik pozícióban van, -1 ha nincs benne
+                count++;
+            }
+        }
+        return count;
+    }
+
+//A paraméterként adott Stringben minden magánhangzót alakítsunk nagybetűsre!
+    public static String swap(String st){
+        String vowels = "aeiou";
+        String newString = "";
+        for (int i=0; i<st.length(); i++) {
+            char letter = st.charAt(i);
+            if (vowels.indexOf(letter)>=0) {
+                newString += Character.toUpperCase(letter);
+            } else {
+                newString += letter;
+            }
+        }
+        return newString;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class MainTest {
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Repeatable;
+
+public class MainTest {
+
+    @Test
+    public void TestCountConsonent() {
+        Main main = new Main();
+        int actualResult = main.countConsonent("gfghjkl");
+        int expectedResult = 7;                 // a space-t is beleszámoljuk!!! mint nem magánhangzót
+
+        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(3, main.countConsonent("Alibaba"));
+    }
+
+    @Test
+    public void swap() {
+        Main main = new Main();
+        String actual = main.swap("Hello World!");
+        String expected = "HEllO WOrld!";
+
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals("AlIbAbA", main.swap("Alibaba"));
+    }
 }
